eeddfb0f19f8299fb3f2c45d73bc5ec677478d68
mdfonlyyou <33276760+mdfonlyyou@users.noreply.github.com>
2018-06-19 11:19:00 +0800

    Update Swagger2Config.java
    修改

--- ServiceCharger/src/main/java/com/midutek/charger/config/Swagger2Config.java
+++ ServiceCharger/src/main/java/com/midutek/charger/config/Swagger2Config.java
@@ -34,7 +34,7 @@ public class Swagger2Config {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("马登峰的文档")
-                .description("厕所死死死死死")
+                .description("API测试文档.....")
                 .termsOfServiceUrl("http://www.midutek.com/index.html")
                 .contact(new Contact("madengfeng",null,null))
                 .version("1.0")
